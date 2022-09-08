1ff98f2993827f836873f67884644bbe86f83598
lukaszfronia <86720750+lukaszfronia@users.noreply.github.com>
2022-09-09 01:50:04 +0200

    create functional function for mobile navigation

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
 
 const header = document.querySelector(".header");
-
+const btnMobile = document.querySelector(".btn-mobile-nav");
+const btns = document.querySelectorAll("a:link");
 const navHeight = header.getBoundingClientRect().height;
 const sectionHero = document.querySelector(".section-hero");
+
+const year = document.querySelector(".year");
+
+year.innerHTML = new Date().getFullYear();
 
 const options = {
   root: null,
@@ -36,3 +41,24 @@ const writeText = () => {
   }
 };
 setInterval(writeText, 100);
+
+const opnClsMenu = () => header.classList.toggle("nav-open");
+btnMobile.addEventListener("click", opnClsMenu);
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = btn.getAttribute("href");
+
+    if (href === "#") window.scrollTo({ top: 20, behavior: "smooth" });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (btn.classList.contains("main-nav-link")) {
+      opnClsMenu();
+    }
+  })
+);
